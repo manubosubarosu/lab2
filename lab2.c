b44c1de23523dc57e0b6ae1c6fd9e09fcdea5beb
Erhan Emanuel <emanuelerhan53@gmail.com>
2026-03-03 11:16:07 +0200

    Adauga lab2.c

--- lab2.c
+++ lab2.c
@@ -2,16 +2,17 @@
 #include <stdio.h>
 
 int function(int x){
-	return x=x-2;
+    x=x-2;
+    x=function(x);
+    return x;
 }
 
 int main(){
-	int a=30, i=0;
-	
-	while (a>0){
-		a=function(a);
-		i++;
-	}
-	printf("%d", i);
-	return 0;	
+    int a=30, i=0;
+    while (a>0){
+        a=function(a);
+        i++;
+    }
+    printf("%d", i);
+    return 0;
 }
